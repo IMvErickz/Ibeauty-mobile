a942824186051b6b742c80c095e07130d642cb38
IMvErickz <erickspy2003@gmail.com>
2022-11-26 20:07:06 -0300

    create project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+//import { Text, View } from 'react-native';
+
+import {NativeBaseProvider, Text, VStack} from 'native-base'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <VStack className='flex-1 bg-gray-700 justify-center items-center'>
+      <Text className="text-6xl font-bold">Hello</Text>
+    </VStack>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
